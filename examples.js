c35805e9a221f1406dae4d96cdc930081f2c55c2
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-06-12 21:11:16 +0300

    Add Javascript files

---
+++ examples.js
@@ -0,0 +1,56 @@
+var person = {
+    employed: true,
+    age: 29,
+    name: 'First Last',
+    skills: [
+      'html',
+      "css",
+      `js`
+    ]
+  };
+  
+  console.debug(person);
+  console.info(person.name);
+  console.debug(person.age);
+  console.debug(person.employed);
+  console.debug(person["employed"]); // same as above
+
+  // function, name, parameters, code block
+function emptyFn() {}
+
+function getWelcomeMsg(name) {
+  var msg = "Hello " + name + ", " +
+  "welcome at FastTrackIT.";
+  return msg;
+}
+
+function getMsg(name) {
+  return `Hey ${name}, I'm glad to help you learn js.`;
+}
+
+// call getWelcomeMsg and store result in variable
+var welcomeNick = getWelcomeMsg("Nick");
+var welcomeJohn = getWelcomeMsg("John");
+
+console.info(welcomeNick);
+
+var person = {
+    age: 29,
+    name: 'Nick',
+    learn: function() {
+      console.info("I'm learning JS, I love it!");
+    },
+    play: function() {
+      console.info("I'm playing. My name is ", this.name);
+    }
+  };
+  
+  person.learn();
+  person.play();
+  
+  //typeof document
+  //typeof document.getElementById
+  //typeof console
+  console.info(person["name"]); // => Nick
+  var action = "learn";
+  person[action]();
