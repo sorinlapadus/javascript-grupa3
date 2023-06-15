710602e20f2a05a0d962236c60bdc66eba82764a
sorin.lapadus <sorin.lapadus@indsoft.ro>
2023-06-15 18:17:42 +0300

    Apply code formtter

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function hideAllPages() {
-    hide('home');
-    hide('languages');
-    hide('projects');
-    hide('skills');
+  hide("home");
+  hide("languages");
+  hide("projects");
+  hide("skills");
 }
 
 function displayPage(id) {
-    hideAllPages();
-    var page = document.getElementById(id);
-    page.style.display = 'block';
+  hideAllPages();
+  var page = document.getElementById(id);
+  page.style.display = "block";
 }
 
 function hide(id) {
-    var page = document.getElementById(id);
-    page.style.display = 'none';
+  var page = document.getElementById(id);
+  page.style.display = "none";
 }
 
-displayPage('home');
+displayPage("home");
